708ec556fc6bc2d02d77c7f27cec8a1ca964d7bf
jeffyjo <jeff_1992_1992@hotmail.com>
2017-03-07 14:33:58 +0100

    search component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
---
+++ src/app/search-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormBuilder, Validators,
+  FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'search-form',
+  styleUrls: ['./css/search-form.css'],
+  templateUrl: './search-form.template.html'
+})
+
+export class SearchFormComponent implements OnInit {
+
+    ngOnInit(){}
+}
+
